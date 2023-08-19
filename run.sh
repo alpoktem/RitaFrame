1fda14fd3cae32530e96f701fb834312282549ef
Alp <alp@collectivat.cat>
2023-08-19 04:32:30 +0200

    set sleep

--- run.sh
+++ run.sh
@@ -5,5 +5,10 @@ sleep 10
 # Start unclutter to hide the cursor
 unclutter -idle 0.1 -root &
 
+#Set screen sleep time (seconds)
+export DISPLAY=:0; xset s on s 120
+
+# TODO: Somehow the sensor thread doesn't work with gunicorn
 # gunicorn app:app --access-logfile access.log --error-logfile error.log
+
 python app.py >> run.log
